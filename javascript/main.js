292e355581a03db089a9a3de950121466fa87038
Diego Del Agua <diego.delagua@hotmail.com>
2023-12-24 20:16:41 -0300

    la-funkeria

---
+++ javascript/main.js
@@ -0,0 +1,195 @@
+//PRODUCTOS
+
+class Producto{
+    constructor(nombre,precio,img, categoria){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.img = img;
+        this.categoria = categoria;
+    }
+}
+
+//MARVEL
+const MARVEL_0001 = new Producto ("Capitan <br> America",4900,"./assets/MARVEL/Captain-america.jpeg","marvel");
+const MARVEL_0002 = new Producto ("Dr. <br> Strange",4600,"./assets/MARVEL/Dr.Strange.jpeg","marvel");
+const MARVEL_0003 = new Producto ("Groot",5000,"./assets/MARVEL/Groot.jpeg","marvel");
+const MARVEL_0004 = new Producto ("Hulk",3900,"./assets/MARVEL/Hulk.jpeg","marvel");
+const MARVEL_0005 = new Producto ("Hulkbuster",4200,"./assets/MARVEL/Hulkbuster.jpeg","marvel");
+const MARVEL_0006 = new Producto ("Ironman",5000,"./assets/MARVEL/Ironman.jpeg","marvel");
+const MARVEL_0007 = new Producto ("Loki",5000,"./assets/MARVEL/Loki.jpeg","marvel");
+const MARVEL_0008 = new Producto ("Magneto",4100,"./assets/MARVEL/Magneto.jpeg","marvel");
+const MARVEL_0009 = new Producto ("Matt",4000,"./assets/MARVEL/Matt.jpeg","marvel");
+const MARVEL_0010 = new Producto ("Bruja <br> Escarlata",4700,"./assets/MARVEL/Scarlet-witch.jpeg","marvel");
+const MARVEL_0011 = new Producto ("Spiderman",4800,"./assets/MARVEL/Spiderman.jpeg","marvel");
+const MARVEL_0012 = new Producto ("Star Lord",3600,"./assets/MARVEL/Star-lord.jpeg","marvel");
+const MARVEL_0013 = new Producto ("Thanos",3800,"./assets/MARVEL/Thanos.jpeg","marvel");
+const MARVEL_0014 = new Producto ("Thor",4000,"./assets/MARVEL/Thor-2.jpeg","marvel");
+const MARVEL_0015 = new Producto ("Thor",4400,"./assets/MARVEL/Thor.jpeg","marvel");
+const MARVEL_0016 = new Producto ("Venom",4500,"./assets/MARVEL/Venom.jpeg","marvel");
+const MARVEL_0017 = new Producto ("Vision",3500,"./assets/MARVEL/Vision.jpeg","marvel");
+const MARVEL_0018 = new Producto ("Wolverine",5000,"./assets/MARVEL/Wolverine.jpeg","marvel");
+
+//GAMER
+const GAMER_0001 = new Producto ("Altair",4000,"./assets/GAMER/Altair.jpeg","gamer");
+const GAMER_0002 = new Producto ("Call of <br> Duty",3600,"./assets/GAMER/COD.jpeg","gamer");
+const GAMER_0003 = new Producto ("Crash",4300,"./assets/GAMER/Crash.jpeg","gamer");
+const GAMER_0004 = new Producto ("Cuphead",4400,"./assets/GAMER/Cuphead.jpeg","gamer");
+const GAMER_0005 = new Producto ("Freedy",5000,"./assets/GAMER/Freedy.jpeg","gamer");
+const GAMER_0006 = new Producto ("Goro",3900,"./assets/GAMER/Goro.jpeg","gamer");
+const GAMER_0007 = new Producto ("Jinx",5000,"./assets/GAMER/Jinx.jpeg","gamer");
+const GAMER_0008 = new Producto ("Ken",4600,"./assets/GAMER/Ken.jpeg","gamer");
+const GAMER_0009 = new Producto ("Lee-Sin",4900,"./assets/GAMER/Lee-sin.jpeg","gamer");
+const GAMER_0010 = new Producto ("Megaman",4000,"./assets/GAMER/Megaman.jpeg","gamer");
+const GAMER_0011 = new Producto ("Mileena",4000,"./assets/GAMER/Mileena.jpeg","gamer");
+const GAMER_0012 = new Producto ("Ms. <br> Pac-man",3600,"./assets/GAMER/Ms.Pac-man.jpeg","gamer");
+const GAMER_0013 = new Producto ("Peely",4000,"./assets/GAMER/Peely.jpeg","gamer");
+const GAMER_0014 = new Producto ("Ryu",4900,"./assets/GAMER/Ryu.jpeg","gamer");
+const GAMER_0015 = new Producto ("Scorpion",5000,"./assets/GAMER/Scorpion.jpeg","gamer");
+const GAMER_0016 = new Producto ("Sonic",5000,"./assets/GAMER/Sonic.jpeg","gamer");
+const GAMER_0017 = new Producto ("Thrall",5000,"./assets/GAMER/Thrall.jpeg","gamer");
+const GAMER_0018 = new Producto ("VI",4100,"./assets/GAMER/VI.jpeg","gamer");
+
+//DISNEY
+const DISNEY_0001 = new Producto ("Ariel",4000,"./assets/DISNEY/Ariel.jpeg","disney");
+const DISNEY_0002 = new Producto ("Bella",4200,"./assets/DISNEY/Belle.jpeg","disney");
+const DISNEY_0003 = new Producto ("Cenicienta",3000,"./assets/DISNEY/Cinderella.jpeg","disney");
+const DISNEY_0004 = new Producto ("Jack <br> Sparrow",4900,"./assets/DISNEY/Jack-sparrow.jpeg","disney");
+const DISNEY_0005 = new Producto ("Lumiere",3500,"./assets/DISNEY/Lumiere.jpeg","disney");
+const DISNEY_0006 = new Producto ("Maléfica",4200,"./assets/DISNEY/Maleficent.jpeg","disney");
+const DISNEY_0007 = new Producto ("Merida",4400,"./assets/DISNEY/Merida.jpeg","disney");
+const DISNEY_0008 = new Producto ("Mike",5000,"./assets/DISNEY/Mike.jpeg","disney");
+const DISNEY_0009 = new Producto ("Minnie",3400,"./assets/DISNEY/Minnie.jpeg","disney");
+const DISNEY_0010 = new Producto ("Moana",4000,"./assets/DISNEY/Moana.jpeg","disney");
+const DISNEY_0011 = new Producto ("Pinocchio",4700,"./assets/DISNEY/Pinocchio.jpeg","disney");
+const DISNEY_0012 = new Producto ("Ratatouille",4100,"./assets/DISNEY/Ratatouille.jpeg","disney");
+const DISNEY_0013 = new Producto ("Simba",4600,"./assets/DISNEY/Simba.jpeg","disney");
+const DISNEY_0014 = new Producto ("Stitch",3600,"./assets/DISNEY/Stitch.jpeg","disney");
+const DISNEY_0015 = new Producto ("Sulley",5000,"./assets/DISNEY/Sulley.jpeg","disney");
+const DISNEY_0016 = new Producto ("Campanita",4000,"./assets/DISNEY/Tinkerbell.jpeg","disney");
+const DISNEY_0017 = new Producto ("Winnie Poo",4600,"./assets/DISNEY/Winnie-the-poo.jpg","disney");
+const DISNEY_0018 = new Producto ("Woody",5000,"./assets/DISNEY/Woody.jpeg","disney");
+
+//DC
+const DC_0001 = new Producto ("Batman Red",4700,"./assets/DC/Batman-red.jpeg","dc");
+const DC_0002 = new Producto ("Batman",5000,"./assets/DC/Batman.jpeg","dc");
+const DC_0003 = new Producto ("Catwoman",4000,"./assets/DC/Catwoman.jpeg","dc");
+const DC_0004 = new Producto ("Cyborg",3000,"./assets/DC/Cyborg.jpeg","dc");
+const DC_0005 = new Producto ("Darkeid",3800,"./assets/DC/Darkseid.jpeg","dc");
+const DC_0006 = new Producto ("Flash",4000,"./assets/DC/Flash.jpeg","dc");
+const DC_0007 = new Producto ("Harley <br> Queen",4100,"./assets/DC/Harley-queen-2.jpeg","dc");
+const DC_0008 = new Producto ("Harley <br> Queen",4500,"./assets/DC/Harley-queen.jpeg","dc");
+const DC_0009 = new Producto ("Joker",4900,"./assets/DC/Joker-2.jpeg","dc");
+const DC_0010 = new Producto ("Joker",4800,"./assets/DC/Joker.jpeg","dc");
+const DC_0011 = new Producto ("Kid Flash",3500,"./assets/DC/Kid-flash.jpeg","dc");
+const DC_0012 = new Producto ("Martian",4000,"./assets/DC/Martian.jpeg","dc");
+const DC_0013 = new Producto ("Hiedra <br> Venenosa",3200,"./assets/DC/Poison-ivy.jpeg","dc");
+const DC_0014 = new Producto ("Redhood",4000,"./assets/DC/Redhood.jpeg","dc");
+const DC_0015 = new Producto ("Robin",4200,"./assets/DC/Robin.jpeg","dc");
+const DC_0016 = new Producto ("Supergirl",4100,"./assets/DC/Supergirl.jpeg","dc");
+const DC_0017 = new Producto ("Superman",5000,"./assets/DC/Superman.jpeg","dc");
+const DC_0018 = new Producto ("Linterna <br> Blanca",3900,"./assets/DC/White-lantern.jpeg","dc");
+
+//ANIME
+const ANIME_0001 = new Producto ("Asuka",3000,"./assets/ANIME/Asuka.jpeg","anime");
+const ANIME_0002 = new Producto ("Broly",4000,"./assets/ANIME/Broly.jpeg","anime");
+const ANIME_0003 = new Producto ("Eren",4800,"./assets/ANIME/Eren.jpg","anime");
+const ANIME_0004 = new Producto ("Erwin",4000,"./assets/ANIME/Erwin.jpeg","anime");
+const ANIME_0005 = new Producto ("Eva 01",4200,"./assets/ANIME/Eva01.jpeg","anime");
+const ANIME_0006 = new Producto ("Freezer",3400,"./assets/ANIME/Freezer.jpeg","anime");
+const ANIME_0007 = new Producto ("Goku",3800,"./assets/ANIME/Goku.jpeg","anime");
+const ANIME_0008 = new Producto ("Gotenks 3",3500,"./assets/ANIME/Gotenks-3.jpeg","anime");
+const ANIME_0009 = new Producto ("Ichigo",4000,"./assets/ANIME/Ichigo.jpeg","anime");
+const ANIME_0010 = new Producto ("Kakashi",4200,"./assets/ANIME/Kakashi.jpeg","anime");
+const ANIME_0011 = new Producto ("Levi",5000,"./assets/ANIME/Levi.jpeg","anime");
+const ANIME_0012 = new Producto ("Majin Boo",3600,"./assets/ANIME/Majin-boo.jpeg","anime");
+const ANIME_0013 = new Producto ("Naruto",3400,"./assets/ANIME/Naruto.jpeg","anime");
+const ANIME_0014 = new Producto ("Nezuko",3800,"./assets/ANIME/Nezuko.jpeg","anime");
+const ANIME_0015 = new Producto ("Rei",3900,"./assets/ANIME/Rei.jpeg","anime");
+const ANIME_0016 = new Producto ("Satoru",4800,"./assets/ANIME/Satoru.jpeg","anime");
+const ANIME_0017 = new Producto ("Tanjiro",3000,"./assets/ANIME/Tanjiro.jpeg","anime");
+const ANIME_0018 = new Producto ("Vegeta <br> Blue",5000,"./assets/ANIME/Vegeta-blue.jpg","anime");
+
+const PRODUCTOS = [MARVEL_0001,MARVEL_0005,MARVEL_0002,MARVEL_0003,MARVEL_0004,MARVEL_0006,MARVEL_0007,MARVEL_0008,MARVEL_0009,MARVEL_0010,MARVEL_0011,
+    MARVEL_0012,MARVEL_0013,MARVEL_0014,MARVEL_0015,MARVEL_0016,MARVEL_0017,MARVEL_0018,GAMER_0001,GAMER_0002,GAMER_0003,GAMER_0004,GAMER_0005,GAMER_0006,
+    GAMER_0007,GAMER_0008,GAMER_0009,GAMER_0010,GAMER_0011,GAMER_0012,GAMER_0013,GAMER_0014,GAMER_0015,GAMER_0016,GAMER_0017,GAMER_0018,DISNEY_0001,DISNEY_0002,
+    DISNEY_0003,DISNEY_0004,DISNEY_0005,DISNEY_0006,DISNEY_0007,DISNEY_0008,DISNEY_0009,DISNEY_0010,DISNEY_0011,DISNEY_0012,DISNEY_0013,DISNEY_0014,DISNEY_0015,
+    DISNEY_0016,DISNEY_0017,DISNEY_0018,DC_0001,DC_0002,DC_0003,DC_0004,DC_0005,DC_0006,DC_0007,DC_0008,DC_0009,DC_0010,DC_0011,DC_0012,DC_0013,DC_0014,
+    DC_0015,DC_0016,DC_0017,DC_0018,ANIME_0001,ANIME_0002,ANIME_0003,ANIME_0004,ANIME_0005,ANIME_0006,ANIME_0007,ANIME_0008,ANIME_0009,ANIME_0010,ANIME_0011,
+    ANIME_0012,ANIME_0013,ANIME_0014,ANIME_0015,ANIME_0016,ANIME_0017,ANIME_0018];
+
+
+const CARGAR_PRODUCTOS = document.querySelector("#contenedor");
+const BOTONES_CATEGORIA = document.querySelectorAll (".botones-categoria");
+const CARRITO = []; 
+
+
+function cargarProductos (productosElegidos) {
+
+    CARGAR_PRODUCTOS.innerHTML = "";
+
+    productosElegidos.forEach (producto => {
+        const div = document.createElement("div");
+
+        div.className="card";
+        div.innerHTML =`
+                    <img class="card-img" src=".${producto.img}" alt="">
+                    <div class="card-info">
+                        <h3 class="nombre-item">${producto.nombre}</h3>
+                        <p class="precio-item">$ ${producto.precio}</p>
+                        <button class="boton">Agregar al Carrito</button>
+                    </div>
+                `;
+                CARGAR_PRODUCTOS.appendChild(div);
+
+    });
+}
+
+
+
+BOTONES_CATEGORIA.forEach (boton => {
+        boton.addEventListener ("click",(e)=> {
+            e.currentTarget.classList.add ("active");
+
+            const PRODUCTOS_BOTON = PRODUCTOS.filter (producto => ((producto.categoria).toLowerCase()) === e.currentTarget.id);
+
+            cargarProductos (PRODUCTOS_BOTON);
+            console.log ("hola");
+
+    })
+});
+
+
+function generarNumeroAleatorio(min, max) {
+    // Genera un número aleatorio en el rango [0, 1) y lo ajusta al rango deseado
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+const PRODUCTOS_INDEX = document.querySelector(".todosProductos");
+
+function cargarIndex (PRODUCTOS) {
+
+    PRODUCTOS_INDEX.innerHTML = "";
+    let x; 
+    for (x=0; x < 6; x++){ 
+        i= generarNumeroAleatorio (0 ,89);
+        const DIV_INDEX = document.createElement ("div");
+        
+        DIV_INDEX.className="card";
+        DIV_INDEX.innerHTML =`
+                    <img class="card-img" src="${PRODUCTOS[i].img}" alt="">
+                    <div class="card-info">
+                        <h3 class="nombre-item">${PRODUCTOS[i].nombre}</h3>
+                        <p class="precio-item">$ ${PRODUCTOS[i].precio}</p>
+                        <button class="boton">Agregar al Carrito</button>
+                    </div>
+                `;
+                PRODUCTOS_INDEX.appendChild(DIV_INDEX);
+    }
+}
+  
+
+cargarIndex(PRODUCTOS);
+cargarProductos(PRODUCTOS);
+    
+
